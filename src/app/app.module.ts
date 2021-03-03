4118c1d5642ab7193e7bce1474b2ea40cef60012
Louis-Marie <louismarie.merminod@gmail.om>
2021-03-03 08:50:01 +0100

    Create home, login & profile components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
+    HomeComponent,
+    LoginComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,

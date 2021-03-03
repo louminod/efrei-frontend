bccaeec017fe28fd8630288b04775b915f5f97a4
Louis-Marie <louismarie.merminod@gmail.om>
2021-03-03 09:01:27 +0100

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,12 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from "@angular/material/toolbar";
-import { NavigationComponent } from './navigation/navigation.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { ProfileComponent } from './profile/profile.component';
+import {NavigationComponent} from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    HomeComponent,
-    LoginComponent,
-    ProfileComponent,
   ],
   imports: [
     BrowserModule,
